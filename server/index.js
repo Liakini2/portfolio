4f0c5fc3f968bb5899b7c4a65d7992a5bbcdc75f
Lia Couch <76275077+Liakini2@users.noreply.github.com>
2021-04-26 13:26:44 -0600

    ready to host

---
+++ server/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const path = require('path')
+
+const {SERVER_PORT} = process.env
+
+app.use(express.json())
+app.use(express.static(__dirname + '/../build'))
+app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname + '../build'))
+})
+app.listen(SERVER_PORT, ()=> console.log(`Server running on port ${SERVER_PORT}`))
